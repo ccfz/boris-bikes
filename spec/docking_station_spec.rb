41da71776c4fae5b1e564c9b94d2da5492cd07de
Claudia Green <claudia@claudiag.me.uk>
2016-03-03 11:36:35 +0000

    Isolated tests with Doubles, and mocked behaviour on to doubles. Tests passing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,9 @@ require "docking_station"
 
 describe DockingStation do
 
+  let(:bike) { double(:bike) }
   describe 'initialization' do
     subject { DockingStation.new }
-    let(:bike) { Bike.new }
     it 'defaults capacity' do
       described_class::DEFAULT_CAPACITY.times do
       subject.dock(bike)
@@ -14,15 +14,15 @@ describe DockingStation do
 
     it "accepts a given capacity" do
       docking_station = DockingStation.new(30)
-      30.times { docking_station.dock Bike.new }
-      expect{ docking_station.dock Bike.new}.to raise_error "The docking station is full"
+      30.times { docking_station.dock (bike) }
+      expect{ docking_station.dock (bike)}.to raise_error "The docking station is full"
     end
   end
 
   it { is_expected.to respond_to(:release_bike) }
   describe "release_bike" do
     it "releases bike" do
-      bike = Bike.new
+      bike = double(:bike, broken?: false)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -33,7 +33,8 @@ describe DockingStation do
   end
 
   it "expects bikes to be working" do
-    bike = Bike.new
+    bike = double(:bike, working?: true)
+    allow(bike).to receive(:broken?)
     subject.dock(bike)
     expect(subject.release_bike).to be_working
   end
@@ -41,18 +42,16 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   describe "#dock" do
     it "docks something" do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq bike
     end
 
     it "raises an error" do
-      subject.capacity.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error "The docking station is full"
+      subject.capacity.times { subject.dock(bike) }
+      expect { subject.dock(bike) }.to raise_error "The docking station is full"
     end
   end
 
   it "bike properly docked" do
-    bike = Bike.new
     subject.dock(bike)
     expect(subject.bikes[-1]).to eq bike
   end
